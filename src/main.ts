9025d0613b232ac35ada0d032a0431591ac31199
Ala'a Ayoubi <lola.ayoubi.ah@gmail.com>
2025-02-16 14:49:38 +0300

    .prettierrc

--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,24 @@
+/* eslint-disable prettier/prettier */
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { LoggingInterceptor } from './common/interceptors/logging/logging.interceptor';
 import { HttpExceptionFilter } from './common/filters/http-exception/http-exception.filter';
 import { ValidationPipe } from '@nestjs/common';
 
-
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // Global Interceptor for Logging
   app.useGlobalInterceptors(new LoggingInterceptor());
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
       whitelist: true,
-    })
-  )
+    }),
+  );
 
-  // Global HTTP Exception Filter
   app.useGlobalFilters(new HttpExceptionFilter());
 
   await app.listen(3000);
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
-bootstrap();
+void bootstrap();
